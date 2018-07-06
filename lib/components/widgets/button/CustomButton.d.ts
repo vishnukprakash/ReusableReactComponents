53fe1a186966af11af1abb5636a0aa71331c4dfa
vishnukprakash <vishnuprakash323@gmail.com>
2018-07-06 16:25:07 +0530

    css removed

--- lib/components/widgets/button/CustomButton.d.ts
+++ lib/components/widgets/button/CustomButton.d.ts
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import './CustomButton.css';
 export declare class CustomButton extends React.Component<any, any> {
     render(): JSX.Element;
 }
--- src/components/widgets/button/CustomButton.tsx
+++ src/components/widgets/button/CustomButton.tsx
@@ -1,8 +1,9 @@
 import *  as React from 'react';
-import './CustomButton.css';
 
 export class CustomButton extends React.Component<any, any>{
     public render(){
-        return(<button style={{height: this.props.height, width: this.props.width, backgroundColor: '#ff0f97'}}/>);
+        return(
+                <button style={{height: this.props.height, width: this.props.width}}/>
+            );
     }
 }
