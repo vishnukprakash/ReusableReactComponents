027c505c60e375dde3d027b8e4c80eea8bfa14fa
vishnukprakash <vishnuprakash323@gmail.com>
2018-07-06 16:00:49 +0530

    types def added

---
+++ lib/components/widgets/button/CustomButton.d.ts
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import './CustomButton.css';
+export declare class CustomButton extends React.Component<any, any> {
+    render(): JSX.Element;
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { CustomButton } from './components/widgets/button/CustomButton';
+import { CustomSelectItem } from './components/widgets/selectitem/CustomSelectItem';
+import { CustomTextItem } from './components/widgets/textitem/CustomTextItem';
+export { CustomButton };
+export { CustomSelectItem };
+export { CustomTextItem };
